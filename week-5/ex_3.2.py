a37bac4cdc1921b3c078104eeb67eb77e2bf99fd
Alessia-Barlascini <alessia.barlascini.89@gmail.com>
2019-07-07 09:16:18 +0200

    program working

---
+++ week-5/ex_3.2.py
@@ -0,0 +1,16 @@
+# calculate pay
+# focus on not valid input
+
+
+hrs = input("Enter Hours: ")
+pay = input("Enter Pay: ")
+try:
+    p = float(pay)
+    h = float(hrs)
+except:
+    print("ERROR: Please, enter a valid number")
+
+if h <= 40:
+    print(p * h)
+else:
+    print(40 * p + (h - 40) * p * 1.5)
