a61e64904bce36d05c782208cb5327c6d1910471
Alessia-Barlascini <alessia.barlascini.89@gmail.com>
2019-07-07 09:28:55 +0200

    hello

---
+++ week-6/parameters.py
@@ -0,0 +1,14 @@
+# say hello in different languages
+
+def hello (lang):
+    if lang == "en":
+        print("goodmorning")
+    if lang == "fr":
+        print("bonjour")
+    if lang == "de":
+        print("Guten Tag")
+
+
+hello("en")
+hello("fr")
+hello("de")
