b84bc91917a81cf57c7fbc903a278eb995d58443
Alessia-Barlascini <alessia.barlascini.89@gmail.com>
2019-07-11 19:36:21 +0200

    statistics

--- Data-Structures/week-3/ex_7.2.py
+++ Data-Structures/week-3/ex_7.2.py
@@ -4,8 +4,22 @@
 #Count these lines and extract the floating point values
 #compute the average of those values and produce an output
 
+from statistics import mean
+memo=[]
 fname=input('Enter name file: ')
 fhand=open(fname)
 for line in fhand:
     line=line.rstrip()
-    print(line)
+    # print(line) - check if the file is right
+    if line.startswith('X-DSPAM-Confidence:'):      #find line stanting with ....
+        line=line[line.find('0'):]                  #extract numerical part
+        line=float(line)                            #tranform into float
+        line= memo.append(line)                     #tranform line into a list
+#print (memo)
+#print(len(memo))
+#print (line)
+average=mean(memo)
+print('Average spam confidence:',average)
+
+
+
