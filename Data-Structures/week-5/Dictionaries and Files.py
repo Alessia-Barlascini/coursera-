c4671bbeac421779d82180680eb1ee8171e7c76d
Alessia-Barlascini <alessia.barlascini.89@gmail.com>
2019-07-15 06:52:22 +0200

    better

--- Data-Structures/week-5/Dictionaries and Files.py
+++ Data-Structures/week-5/Dictionaries and Files.py
@@ -1,15 +1,14 @@
 # create an empty dictionary
 counts={}
 print('Enter a line of text: ')
-line = input('')
+line = open(input(''))
 #create the list
-words = line.split(' ')
-
-
+for lines in line:
+    words = lines.split()
 
 # add the element of the list to the empty dictionary and count them
-for word in words:
-    counts[word]=counts.get(word,0)+1
-print('Counts',counts)
+    for word in words:
+        counts[word]=counts.get(word,0)+1
+print('Counts',counts)      # print the dictionary
 for k,v in counts.items():
-    print (k,v)
+    print (k,v)             # print the element of the dictionary
