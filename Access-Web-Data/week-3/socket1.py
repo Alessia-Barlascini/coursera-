f9a27e8d10b1515caa9da734597b6f1f403bba08
Alessia-Barlascini <alessia.barlascini.89@gmail.com>
2019-07-17 07:24:05 +0200

    done

--- Access-Web-Data/week-3/socket1.py
+++ Access-Web-Data/week-3/socket1.py
@@ -1,14 +1,14 @@
 import socket
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                      # create the portal
+mysock.connect(('data.pr4e.org', 80))                                           # find the receiver
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()            # ask for something
+mysock.send(cmd)                                                                # se the request
 
 while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
+    data = mysock.recv(512)                     # ask for the first 512 characters
+    if len(data) < 1:                           # if there's no data break
         break
-    print(data.decode(),end='')
+    print(data.decode())                 # print the data decoded
 
-mysock.close()
+mysock.close()                                  # close the portal
